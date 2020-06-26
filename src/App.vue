3a4b96e69f9f9fb7df8c7d04533cfe69cde53dbd
hyt <1363592973@qq.com>
2020-06-26 22:20:24 +0800

    测试提交人员

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <el-row>
-      <el-button>默认按钮</el-button>
       <el-button type="primary">主要按钮</el-button>
       <el-button type="success">成功按钮</el-button>
       <el-button type="info">信息按钮</el-button>
