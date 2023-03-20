f5570b20f45818d7e83e19bb76becd648753cb30
heyuting009 <heyuting009@ke.com>
2023-03-20 11:32:26 +0800

    feat: test

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: heyuting009
+ * @Date: 2023-03-20 11:31:11
+ * @LastEditTime: 2023-03-20 11:32:00
+ * @LastEditors: heyuting009
+ * @Description: 文件描述
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -9,3 +16,5 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+// test
+
